a3201644cb00db8a5db7cdd6150b8c1ff083987c
TinuBose <117371766+TinuBose@users.noreply.github.com>
2022-12-30 12:13:36 +0530

    Update largest_among_3_nos.py

--- python_lab/largest_among_3_nos.py
+++ python_lab/largest_among_3_nos.py
@@ -1,15 +1,11 @@
-a1=int(input("enter 1st number"))
-a2=int(input("enter 2nd number"))
-a3=int(input("enter 3rd number"))
-if a1>a2:
-    if  a1>a3:
-        print(a1)
-    else:
-        print(a3)
-elif a2>a3:
-    print(a2)
-else:
-    print(a3)
+lst=[]
+n=int(input("enter number of elements"))
+print("enter elements")
+for i in range(n):
+    ele=int(input())
+    lst.append(ele)
+print("elements = ",lst)
+print("max = ",max(lst))
 
 
 
