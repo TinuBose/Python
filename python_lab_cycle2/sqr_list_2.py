dfc1d2143adec76e37de87ee0b1288c03d5a97e6
TinuBose <117371766+TinuBose@users.noreply.github.com>
2022-12-31 18:10:27 +0530

    Update sqr_list_2.py

--- python_lab_cycle2/sqr_list_2.py
+++ python_lab_cycle2/sqr_list_2.py
@@ -1,7 +1,7 @@
 num_lst = [5555, 1024, 3600, 4444, 9801, 9000]
 sqr_lst = []
 for i in num_lst:
-    for j in range(0, i):
+    for j in range(0, 100):
         if j * j == i:
             sqr_lst.append(i)
 print(sqr_lst)
