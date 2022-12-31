cc2de483774feef2f3c01da65c2b01397215c937
TinuBose <117371766+TinuBose@users.noreply.github.com>
2022-12-31 11:25:44 +0530

    Add files via upload

---
+++ python_lab_cycle2/sqr_list.py
@@ -0,0 +1,10 @@
+li = []
+sqr_lst = []
+n = int(input("enter size"))
+print("enter elements")
+for i in range(n):
+    a = int(input())
+    li.append(a)
+    sqr_lst.append(a * a)
+print("list is ", li)
+print("square list ", sqr_lst)
