a7c1d6429eb9e5623d636be49b5dcb518fd8f75a
TinuBose <117371766+TinuBose@users.noreply.github.com>
2022-12-31 18:11:25 +0530

    Update sqr_list_math_func.py

--- python_lab_cycle2/sqr_list_math_func.py
+++ python_lab_cycle2/sqr_list_math_func.py
@@ -3,7 +3,7 @@ import math
 sqr_list = [5555, 1024, 3600, 4444, 9801, 9000]
 sqr_list2 = []
 for i in sqr_list:
-    for j in range(0, i):
+    for j in range(0, 100):
         a = math.sqrt(i)
         if a == j:
             sqr_list2.append(i)
