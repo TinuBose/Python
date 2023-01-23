e1af8c302d9781b3cffa99f35c932f676a7128e2
TinuBose <117371766+TinuBose@users.noreply.github.com>
2023-01-23 22:14:42 +0530

    Update fibonacci_recursion_.py

--- fibonacci_recursion_.py
+++ fibonacci_recursion_.py
@@ -1,14 +1,15 @@
 def fibonacci(n):
-   if n <= 1:
-       return n
-   else:
-       return(fibonacci(n-1) + fibonacci(n-2))
+    if n <= 1:
+        return n
+    else:
+        return (fibonacci(n - 1) + fibonacci(n - 2))
+
 
 limit = int(input("enter range : "))
 
 if limit <= 0:
-   print("Plese enter a positive integer")
+    print("Plese enter a positive integer")
 else:
-   print("Fibonacci sequence up to ",limit," is ")
-   for i in range(limit):
-       print(fibonacci(i))
+    print("Fibonacci sequence up to ", limit, " is ", end=" ")
+    for i in range(limit):
+        print(fibonacci(i), end=" ")
